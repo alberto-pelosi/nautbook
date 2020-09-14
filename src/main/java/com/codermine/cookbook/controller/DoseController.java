2d3cf15a1fde75b15e262561222f42a8d10b4b8a
apelosi-bb <a.pelosi@codermine.com>
2020-09-14 12:03:02 +0200

    global error handler

--- src/main/java/com/codermine/cookbook/controller/DoseController.java
+++ src/main/java/com/codermine/cookbook/controller/DoseController.java
@@ -2,8 +2,15 @@ package com.codermine.cookbook.controller;
 
 import com.codermine.cookbook.model.Dose;
 import com.codermine.cookbook.service.DoseService;
+import com.fasterxml.jackson.core.JsonParseException;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.HttpStatus;
 import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Error;
 import io.micronaut.http.annotation.Post;
+import io.micronaut.http.hateoas.JsonError;
+import io.micronaut.http.hateoas.Link;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -31,5 +38,15 @@ public class DoseController {
         Dose savedDose = doseService.insertDose(dose);
         logger.debug("DoseController - get - stop - savedDose: {}", savedDose);
         return savedDose;
+    }
+
+    //global error handler
+    @Error(global = true)
+    public HttpResponse<JsonError> jsonError(HttpRequest request, JsonParseException jsonParseException) {
+        JsonError error = new JsonError("Invalid JSON: " + jsonParseException.getMessage())
+                .link(Link.SELF, Link.of(request.getUri()));
+
+        return HttpResponse.<JsonError>status(HttpStatus.BAD_REQUEST, "Fix Your JSON")
+                .body(error);
     }
 }
